1f023e3f18f5678e499c693062c752e432bf3a82
karantec <karanrana3095@gmail.com>
2026-04-14 17:15:50 +0530

    COMMIT

--- app.js
+++ app.js
@@ -40,6 +40,15 @@ app.get("/", (req, res) => {
   res.send("You are connected to Minutos server");
 });
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 /* =======================
    API Routes
 ======================= */
