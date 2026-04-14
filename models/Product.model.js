e4a414efc231cc20a98566c376e9c5bd937615f9
karantec <karanrana3095@gmail.com>
2026-04-14 17:13:02 +0530

    COMMIT

--- models/Product.model.js
+++ models/Product.model.js
@@ -110,7 +110,7 @@ const productSchema = new mongoose.Schema(
     },
     subCategory: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "SubCategory",
+      ref: "subCategory",
       required: true,
     },
 
