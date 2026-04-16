fb18fd8136ea5aba74ed4efe2c871f4d57dcc696
karantec <karanrana3095@gmail.com>
2026-04-16 11:39:37 +0530

    finalcommit

--- models/Product.model.js
+++ models/Product.model.js
@@ -124,7 +124,7 @@ const productSchema = new mongoose.Schema(
     },
     subCategory: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "subCategory",
+      ref: "Subcategory",
       required: true,
     },
 
